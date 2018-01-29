9a29a3edd166026ecedb0dab226092cc56463e13
sangrak8589 <35939739+sangrak8589@users.noreply.github.com>
2018-01-29 18:19:04 -0500

    Add files via upload

--- Labs/Lab2/Mortgages/src/cpsc2150/labs/lab2/Customer.java
+++ Labs/Lab2/Mortgages/src/cpsc2150/labs/lab2/Customer.java
@@ -3,6 +3,10 @@ package cpsc2150.labs.lab2;
 /**
  * Created by kplis on 1/23/2018.
  */
+
+/**
+ * @invariant
+ */
 public class Customer {
     private String name;
 
@@ -11,7 +15,23 @@ public class Customer {
     private int creditScore;
     private Mortgage loan;
 
+    /**
+        Sangram Kadam
+        Jack Tabb
+     * param
+     * return
+     * ensures
+     * requires
+     */
 
+    /**
+     * @param [debt is monthly payments]
+     * @param [inc is income]
+     * @param [score is credit score]
+     * @param [n is name]
+     * @requires 0<= credit <= 850 and debt >= 0 and income >= 0 and name != ""
+     * @ensures debt = debt and income = inc and score = score and name = n
+     */
     Customer( double debt, double inc, int score, String n)
     {
 
@@ -23,6 +43,16 @@ public class Customer {
 
     }
 
+    /**
+     * @param [downPayment is first payment that allows to get a loan]
+     * @param [houseCost is the cost of the house]
+     * @param [years is how many years customer is allowed to repay loan principal + interest]
+     * @requires downPayment >= 0 and houseCost >= 0 and years >=0
+     * @ensures applyForLoan is true iff years <= 30 and downPayment / houseCost > 0.1
+     *          and applyForLoan is false if years > 30 or downPayment / houseCost <= 0.1
+     * @returns applyForLoan is true iff years <= 30 and downPayment / houseCost > 0.1
+     *          applyForLoan is false if years > 30 or downPayment / houseCost <= 0.1
+     */
     public boolean applyForLoan(double downPayment, double houseCost, int years)
     {
         //Take the parameters and apply for a loan. Save the loan details as the customers current loan
@@ -31,6 +61,10 @@ public class Customer {
 
     }
 
+    /**
+     *
+     * @return
+     */
     public double getRate()
     {
         //return the interest rate on the current loan
