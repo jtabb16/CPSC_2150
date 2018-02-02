58ce06656a6f37eaeefaf545912659d1fd58718e
sangrak8589 <35939739+sangrak8589@users.noreply.github.com>
2018-02-02 15:02:30 -0500

    Add files via upload

--- Labs/Lab2/Mortgages/src/cpsc2150/labs/lab2/Customer.java
+++ Labs/Lab2/Mortgages/src/cpsc2150/labs/lab2/Customer.java
@@ -1,7 +1,17 @@
 package cpsc2150.labs.lab2;
 
 /**
- * Created by kplis on 1/23/2018.
+ * Sangram Kadam (sangrak)
+ * Jack Tabb (jbtabb)
+ * CPSC 2150
+ * Kevin Plis
+ * Section 3
+ * 2/2/2018
+ * Lab 2
+ */
+
+/**
+ * @invariant monthlyDebtPayments > 0 and income > 0 and 0 <= creditScore <= 850 and loan has to be an object
  */
 public class Customer {
     private String name;
@@ -12,6 +22,15 @@ public class Customer {
     private Mortgage loan;
 
 
+
+    /**
+     * @param [debt is monthly payments]
+     * @param [inc is income]
+     * @param [score is credit score]
+     * @param [n is name]
+     * @requires 0<= credit <= 850 and debt >= 0 and income >= 0 and name != ""
+     * @ensures debt = debt and income = inc and score = score and name = n
+     */
     Customer( double debt, double inc, int score, String n)
     {
 
@@ -23,6 +42,16 @@ public class Customer {
 
     }
 
+    /**
+     * @param [downPayment is first payment that allows to get a loan]
+     * @param [houseCost is the cost of the house]
+     * @param [years is how many years customer is allowed to repay loan principal + interest]
+     * @requires downPayment >= 0 and houseCost >= 0 and years >=0
+     * @ensures applyForLoan is true iff years <= 30 and downPayment / houseCost > 0.1
+     *          and applyForLoan is false if years > 30 or downPayment / houseCost <= 0.1
+     * @returns applyForLoan is true iff years <= 30 and downPayment / houseCost > 0.1
+     *          applyForLoan is false if years > 30 or downPayment / houseCost <= 0.1
+     */
     public boolean applyForLoan(double downPayment, double houseCost, int years)
     {
         //Take the parameters and apply for a loan. Save the loan details as the customers current loan
@@ -31,30 +60,52 @@ public class Customer {
 
     }
 
+    /**
+     * @requires interest rate > 0%
+     * @ensures getRate = rate
+     * @returns the interest rate on current loan
+     */
     public double getRate()
     {
         //return the interest rate on the current loan
         return 0;
     }
-
+    /**
+     * @requires monthly pay > 0
+     * @ensures getMonthlyPay = monthlyPayment
+     * @returns the monthly payment on the current loan
+     */
     public double getMonthlyPay()
     {
         //return the monthly payment on the current loan
         return 0;
     }
 
+    /**
+     * @requires monthly debt payments > 0
+     * @ensures getMonthlyDebtPayments = monthlyDebtPayment
+     * @returns the customers monthly debt payment
+     */
     public double getMonthlyDebtPayments()
     {
         //return the customers monthly debt payments
         return 0;
     }
-
+    /**
+     * @requires income > 0
+     * @ensures getIncome = income
+     * @returns the customers income
+     */
     public double getIncome()
     {
         //return the customers income
         return 0;
     }
-
+    /**
+     * @requires monthly debt payments > 0
+     * @ensures getCreditScore = creditScore
+     * @returns the customers credit score
+     */
     public int getCreditScore()
     {
         //return the customers credit score
@@ -62,6 +113,11 @@ public class Customer {
     }
 
 
+    /**
+     * @requires name, income, creditScore, and monthlyDebtPayments
+     * @ensures [information about Customer object]
+     * @returns [information about the customer and loan]
+     */
     @Override
     public String toString()
     {
